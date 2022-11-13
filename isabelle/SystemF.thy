5ed30172e237119489123a1bfe61100a34ac8977
Jeremy G. Siek <jeremy.siek@gmail.com>
2022-11-13 16:23:18 -0500

    check

--- isabelle/SystemF.thy
+++ isabelle/SystemF.thy
@@ -49,15 +49,15 @@ interpretation type_sub: subst1 TVar lift_sub\<^sub>t sub\<^sub>t ren\<^sub>t se
   apply (simp add: lift_sub\<^sub>t_def ren\<^sub>t_def)
   done
 
-lemma rename_sub_ren: "rename\<^sub>t \<rho> A = sub\<^sub>t (ren\<^sub>t \<rho>) A"
+lemma rename_sub_ren\<^sub>t: "rename\<^sub>t \<rho> A = sub\<^sub>t (ren\<^sub>t \<rho>) A"
   by (induction A arbitrary: \<rho>) (auto simp add: ren\<^sub>t_def)
 
-lemma lift_def: "\<Up>\<^sub>t \<sigma> x = sub\<^sub>t (ren\<^sub>t Suc) (\<sigma> x)"
-  using rename_sub_ren ren\<^sub>t_def lift_sub\<^sub>t_def by simp
+lemma lift_def\<^sub>t: "\<Up>\<^sub>t \<sigma> x = sub\<^sub>t (ren\<^sub>t Suc) (\<sigma> x)"
+  using rename_sub_ren\<^sub>t ren\<^sub>t_def lift_sub\<^sub>t_def by simp
 
 interpretation type_sub: subst2 TVar lift_sub\<^sub>t sub\<^sub>t ren\<^sub>t seq\<^sub>t
   apply unfold_locales
-  using lift_def apply blast
+  using lift_def\<^sub>t apply blast
   apply simp
   done
 
@@ -144,15 +144,166 @@ interpretation interp_sub: subst6 TVar lift_sub\<^sub>t sub\<^sub>t ren\<^sub>t 
 abbreviation subst_one\<^sub>t :: "Type \<Rightarrow> Type \<Rightarrow> Type" ("_[_]\<^sub>t" 70) where
   "subst_one\<^sub>t A B \<equiv> sub\<^sub>t (B \<bullet> id\<^sub>t) A"
 
+inductive wft :: "nat \<Rightarrow> Type \<Rightarrow> bool" where
+  wft_var[intro!]: "x < n \<Longrightarrow> wft n (TVar x)" |
+  wft_bool[intro!]: "wft n TBool" |
+  wft_fun[intro!]: "\<lbrakk> wft n A; wft n B \<rbrakk> \<Longrightarrow> wft n (A \<rightarrow> B)" |
+  wft_all[intro!]: "wft (Suc n) A \<Longrightarrow> wft n (\<forall> A)"
+
+inductive_cases
+  wftvar[elim!]: "wft n (TVar x)" and
+  wftfun[elim!]: "wft n (A \<rightarrow> B)" and
+  wftall[elim!]: "wft n (\<forall> A)" 
+
+definition wf_ren\<^sub>t :: "Renaming \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> bool" where
+  "wf_ren\<^sub>t \<rho> \<Gamma> \<Gamma>' \<equiv> \<forall> x. x < \<Gamma> \<longrightarrow> \<rho> x < \<Gamma>'"
+declare wf_ren\<^sub>t_def[simp]
+
+(* Node: need equations about renaming! *)
+lemma ext_wf_rent: assumes 1: "wf_ren\<^sub>t \<rho> \<Gamma> \<Gamma>'"
+  shows "wf_ren\<^sub>t (extr \<rho>) (Suc \<Gamma>) (Suc \<Gamma>')"
+  apply auto apply (case_tac x) apply force
+  apply (simp add: lift_ren_def) using 1 apply auto done
+
+lemma wft_rename_pres: "\<lbrakk> wft \<Gamma> A; wf_ren\<^sub>t \<rho> \<Gamma> \<Gamma>' \<rbrakk> \<Longrightarrow> wft \<Gamma>' (rename\<^sub>t \<rho> A)"
+proof (induction A arbitrary: \<Gamma> \<Gamma>' \<rho>)
+  case (TVar x)
+  then show ?case by auto
+next
+  case TBool
+  then show ?case by auto
+next
+  case (TFun A1 A2)
+  then show ?case by auto
+next
+  case (TAll A)
+  then show ?case using ext_wf_rent by auto
+qed
+
+definition wf_sub\<^sub>t :: "Subst\<^sub>t \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> bool" where
+  "wf_sub\<^sub>t \<sigma> \<Gamma> \<Gamma>' \<equiv> \<forall> x. x < \<Gamma> \<longrightarrow> wft \<Gamma>' (\<sigma> x)"
+declare wf_sub\<^sub>t_def[simp]
+
+lemma ext_wf_subt: assumes 1: "wf_sub\<^sub>t \<sigma> \<Gamma> \<Gamma>'"
+  shows "wf_sub\<^sub>t (extt \<sigma>) (Suc \<Gamma>) (Suc \<Gamma>')"
+  apply auto
+proof -
+  fix x assume xg: "x < Suc \<Gamma>" 
+  show "wft (Suc \<Gamma>') ((TVar 0 \<bullet> (\<sigma> ;\<^sub>t \<up>\<^sub>t)) x)"
+  proof (cases x)
+    case 0
+    then show ?thesis by auto
+  next
+    case (Suc y)
+    from xg Suc 1 have 2: "wft \<Gamma>' (\<sigma> y)" by auto
+    from 2 have "wft (Suc \<Gamma>') (rename\<^sub>t Suc (\<sigma> y))"
+      by (rule wft_rename_pres) auto
+    then have "wft (Suc \<Gamma>') (sub\<^sub>t \<up>\<^sub>t (\<sigma> y))" 
+      using rename_sub_ren\<^sub>t[of Suc "\<sigma> y"] by simp
+    with Suc show ?thesis by (simp add: seq\<^sub>t_def)
+  qed
+qed
+  
+lemma wft_sub_pres: "\<lbrakk> wft \<Gamma> A; wf_sub\<^sub>t \<sigma> \<Gamma> \<Gamma>' \<rbrakk> \<Longrightarrow> wft \<Gamma>' (sub\<^sub>t \<sigma> A)"
+proof (induction A arbitrary: \<Gamma> \<Gamma>' \<sigma>)
+  case (TVar x)
+  then show ?case by auto
+next
+  case TBool
+then show ?case by auto
+next
+case (TFun A B)
+  then show ?case by auto
+next
+  case (TAll A)
+  from TAll have 1: "wft (Suc \<Gamma>) A" by blast
+  from TAll have 2: "wf_sub\<^sub>t (extt \<sigma>) (Suc \<Gamma>) (Suc \<Gamma>')"
+    using ext_wf_subt by auto
+  from TAll 1 2 have IH: "wft (Suc \<Gamma>') (sub\<^sub>t (extt \<sigma>) A)" by blast
+  then show ?case by auto
+qed
+
+lemma wft_subst_pres: "\<lbrakk> wft \<Gamma> A; wft \<Gamma> B \<rbrakk> \<Longrightarrow> wft \<Gamma> (A[B]\<^sub>t)"
+  apply (rule wft_sub_pres)
+   apply assumption
+  apply auto
+  apply (case_tac x)
+   apply (auto simp add: id\<^sub>t_def)
+  done
+
 
 section "Terms"
 
 datatype Term =
   Var var
+  | Bool bool
   | App Term Term (infixl "\<cdot>" 70)
   | Lam Term      ("\<lambda>")
   | TLam Term     ("\<Lambda>")
-  | TApp Term Type
+  | TApp Term  (* leave out the type to side-step type substitution *)
+
+inductive wtt :: "Type list \<Rightarrow> nat \<Rightarrow> Term \<Rightarrow> Type \<Rightarrow> bool" ("_;_\<turnstile>_:_" 55) where
+  wt_var[intro!]: "\<Gamma>; \<Sigma> \<turnstile> Var x : \<Gamma>!x" |
+  wt_bool[intro!]: "\<Gamma>; \<Sigma> \<turnstile> Bool b : TBool" |
+  wt_app[intro!]: "\<lbrakk> \<Gamma>; \<Sigma> \<turnstile> L : A \<rightarrow> B; \<Gamma>; \<Sigma> \<turnstile> M : A \<rbrakk> \<Longrightarrow> \<Gamma>; \<Sigma> \<turnstile> L \<cdot> M : B" |
+  wt_lam[intro!]: "\<lbrakk> wft \<Sigma> A; A#\<Gamma>; \<Sigma> \<turnstile> N : B\<rbrakk> \<Longrightarrow> \<Gamma>; \<Sigma> \<turnstile> \<lambda> N : A \<rightarrow> B" |
+  wt_tlam[intro!]: "\<lbrakk> \<Gamma>; Suc \<Sigma> \<turnstile> N : A\<rbrakk> \<Longrightarrow> \<Gamma>; \<Sigma> \<turnstile> \<Lambda> N : \<forall> A" |
+  wt_tapp[intro!]: "\<lbrakk> wft \<Sigma> B; \<Gamma>; \<Sigma> \<turnstile> L : \<forall> A\<rbrakk> \<Longrightarrow> \<Gamma>; \<Sigma> \<turnstile> TApp L : A[B]\<^sub>t" 
+
+fun rename :: "Renaming \<Rightarrow> Term \<Rightarrow> Term" where
+  "rename \<rho> (Var x) = Var (\<rho> x)" |
+  "rename \<rho> (Bool b) = Bool b" |
+  "rename \<rho> (L \<cdot> M) = (rename \<rho> L) \<cdot> (rename \<rho> M)" |
+  "rename \<rho> (\<lambda> N) = \<lambda> (rename (extr \<rho>) N)" |
+  "rename \<rho> (\<Lambda> N) = \<Lambda> (rename \<rho> N)" |
+  "rename \<rho> (TApp L) = TApp (rename \<rho> L)"
+
+type_synonym Subst = "var \<Rightarrow> Term"
+
+definition ids :: "Subst" where
+  "ids x \<equiv> Var x"
+
+definition lift_sub :: "Subst \<Rightarrow> Subst" ("\<Up>") where
+  "\<Up> \<sigma> x \<equiv> rename Suc (\<sigma> x)"
+
+abbreviation exts :: "Subst \<Rightarrow> Subst" where
+  "exts \<sigma> \<equiv> Var 0 \<bullet> \<Up> \<sigma>"
+
+fun sub :: "Subst \<Rightarrow> Term \<Rightarrow> Term" where
+  "sub \<sigma> (Var x) = \<sigma> x" |
+  "sub \<sigma> (Bool b) = Bool b" |
+  "sub \<sigma> (L \<cdot> M) = (sub \<sigma> L) \<cdot> (sub \<sigma> M)" |
+  "sub \<sigma> (\<lambda> N) = \<lambda> (sub (exts \<sigma>) N)" |
+  "sub \<sigma> (\<Lambda> N) = \<Lambda> (sub \<sigma> N)" |
+  "sub \<sigma> (TApp L) = TApp (sub \<sigma> L)"
+
+definition ren :: "Renaming \<Rightarrow> Subst" where
+  "ren \<rho> x \<equiv> Var (\<rho> x)"
+
+definition seq :: "Subst \<Rightarrow> Subst \<Rightarrow> Subst" (infixr ";" 70) where
+  "(\<sigma> ; \<tau>) x \<equiv> sub \<tau> (\<sigma> x)"
+
+interpretation interp_sub: subst1 Var lift_sub sub ren seq
+  apply unfold_locales
+    apply (simp add: ren_def)
+   apply (simp add: seq_def)
+  apply (simp add: lift_sub_def ren_def)
+  done
+
+lemma rename_sub_ren: "rename \<rho> M = sub (ren \<rho>) M"
+  apply (induction M arbitrary: \<rho>)
+  apply (auto simp add: ren_def)
+  done
+
+lemma lift_def: "\<Up> \<sigma> x = sub (ren Suc) (\<sigma> x)"
+  using rename_sub_ren ren_def lift_sub_def apply simp
+  done
+
+interpretation interp_sub: subst2 Var lift_sub sub ren seq
+  apply unfold_locales
+  using lift_def apply blast
+  apply simp
+  done
 
 
 
